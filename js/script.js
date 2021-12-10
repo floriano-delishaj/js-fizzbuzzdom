cbb45b72863a2a8f94953977b5cc5c257a147868
floriano-delishaj <delishaj.floriano@live.it>
2021-12-10 14:48:26 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
+const outputHtml = document.querySelector('.row');
+
+
 for (let i=1; i <= 100; i++) {
 
-    if (i % 3 && i % 5) {
-        console.log(i)
-    } else if (i % 5) {
-        console.log('Fizz')
-    } else if (i % 3) {
-        console.log('Buzz')
+    if (i % 3 === 0 && i % 5 === 0) {
+        outputHtml.innerHTML += `<div class="col py-3">
+                                    <div class="box bg-red d-flex justify-content-center align-items-center font-weight-bold">
+                                        FizzBuzz
+                                    </div>
+                                 </div>`;
+    } else if (i % 5 === 0) {
+        outputHtml.innerHTML += `<div class="col py-3">
+                                    <div class="box bg-yellow d-flex justify-content-center align-items-center font-weight-bold">
+                                        Buzz
+                                    </div>
+                                 </div>`;
+    } else if (i % 3 === 0) {
+        outputHtml.innerHTML += `<div class="col py-3">
+                                    <div class="box bg-aqua d-flex justify-content-center align-items-center font-weight-bold">
+                                        Fizz
+                                    </div>
+                                 </div>`;
     } else {
-        console.log('FizzBuzz')
+        outputHtml.innerHTML += `<div class="col py-3">
+                                    <div class="box bg-blue d-flex justify-content-center align-items-center font-weight-bold">
+                                        ${i}
+                                    </div>
+                                 </div>`;
     }
-    
+
 }
