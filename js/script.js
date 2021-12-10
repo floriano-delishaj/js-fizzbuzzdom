f848aa2fdd1324c71067f48d616701b9fb8552d4
floriano-delishaj <delishaj.floriano@live.it>
2021-12-10 16:51:26 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
 const outputHtml = document.querySelector('.row');
 
+let background;
+let content;
+
 
 for (let i=1; i <= 100; i++) {
 
     if (i % 3 === 0 && i % 5 === 0) {
-        outputHtml.innerHTML += `<div class="col py-3">
-                                    <div class="box bg-red d-flex justify-content-center align-items-center font-weight-bold">
-                                        FizzBuzz
-                                    </div>
-                                 </div>`;
+        content = 'FizzBuz';
+        background = 'bg-red';
     } else if (i % 5 === 0) {
-        outputHtml.innerHTML += `<div class="col py-3">
-                                    <div class="box bg-yellow d-flex justify-content-center align-items-center font-weight-bold">
-                                        Buzz
-                                    </div>
-                                 </div>`;
+        content = 'Buz';
+        background = 'bg-yellow';
     } else if (i % 3 === 0) {
-        outputHtml.innerHTML += `<div class="col py-3">
-                                    <div class="box bg-aqua d-flex justify-content-center align-items-center font-weight-bold">
-                                        Fizz
-                                    </div>
-                                 </div>`;
+        content = 'Fizz';
+        background = 'bg-aqua';
     } else {
-        outputHtml.innerHTML += `<div class="col py-3">
-                                    <div class="box bg-blue d-flex justify-content-center align-items-center font-weight-bold">
-                                        ${i}
-                                    </div>
-                                 </div>`;
+        content = `${i}`;
+        background = 'bg-blue';
     }
+
+
+    outputHtml.innerHTML +=`<div class="col py-3">
+        <div class="box ${background} d-flex justify-content-center align-items-center font-weight-bold">
+            ${content}
+        </div>
+    </div>`;
 
 }
